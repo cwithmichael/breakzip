b4730cc2c7535c997cdfd8c5e7062f77c204e606
Michael <cwithmichael@gmail.com>
2018-02-18 14:24:12 -0600

    working on it

--- break_zip.py
+++ break_zip.py
@@ -2,20 +2,32 @@ from zipfile import ZipFile
 import sys
 
 love = ZipFile(sys.argv[1])
-dictionary = 'dict.txt'
+love.debug = 3
+info = love.infolist()[len(love.infolist())-1]
+dictionary = sys.argv[2]
 password = ''
+test = b'lol'
+zippity = b'PK\x03\x04'
 with open(dictionary, 'r') as dic:
 	for line in dic.readlines():
 		pwd = line.strip('\n')
+		print("Trying: ", pwd)
 		try:
-			love.open(name='Archive.zip',mode='rw', pwd=bytes(pwd, 'utf-8'))
-			love.printdir()
-			#love.read(name='Archive.zip', pwd=pwd)
-			#love.testzip()
-			password = 'Found it!: %s' % pwd
-			break
+			with love.open(info, 'r', pwd=pwd.encode()) as myzip:
+				try:
+					head = myzip.__next__()
+					print(head)
+					if (zippity in head):
+						password = 'Found it!: %s' % pwd
+						break
+				except:
+					pass
+		except RuntimeError:
+			pass
 		except:
-			pass	
+			print(line)
+			print("Unexpected error:", sys.exc_info()[0])
+			raise
 if password:
 	print(password)
 else:
