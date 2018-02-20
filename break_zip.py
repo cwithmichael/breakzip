90902e8ff41bf7aebf33c6f7f1026ee772347804
Michael <cwithmichael@gmail.com>
2018-02-20 16:58:57 -0600

    read a stream from john

--- break_zip.py
+++ break_zip.py
@@ -4,30 +4,30 @@ import sys
 love = ZipFile(sys.argv[1])
 love.debug = 3
 info = love.infolist()[len(love.infolist())-1]
-dictionary = sys.argv[2]
 password = ''
 test = b'lol'
 zippity = b'PK\x03\x04'
-with open(dictionary, 'r') as dic:
-	for line in dic.readlines():
-		pwd = line.strip('\n')
-		print("Trying: ", pwd)
-		try:
-			with love.open(info, 'r', pwd=pwd.strip().encode('utf-8', 'ignore')) as myzip:
-				try:
-					head = myzip.__next__()
-					print(head)
-					if (zippity in head):
-						password = 'Found it!: %s' % pwd
-						break
-				except:
-					pass
-		except RuntimeError:
-			pass
-		except:
-			print(line)
-			print("Unexpected error:", sys.exc_info()[0])
-			raise
+for line in sys.stdin:
+	pwd = line.strip('\n')
+	try:
+		with love.open(info, 'r', pwd=pwd.strip().encode('utf-8', 'ignore')) as myzip:
+			try:
+				head = myzip.__next__()
+				#print("Trying: ", pwd)
+				if (zippity in head):
+					password = 'Found it!: %s' % pwd
+					break
+			except:
+				pass
+	except UnicodeDecodeError:
+		pass
+	except RuntimeError:
+		pass
+	except:
+		print(line)
+		print("Unexpected error:", sys.exc_info()[0])
+		raise
+
 if password:
 	print(password)
 else:
