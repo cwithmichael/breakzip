5a5ad738d254582fc8fb3bcafeca8049fa196426
Michael Terrell <cwithmichael@gmail.com>
2020-12-16 21:59:06 -0600

    chore: fix failing Travis build

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup, find_packages
 
-setup(name='breakzip',
-        version='0.1',
-        description='',
-        author='Michael Terrell',
-        author_email='cwithmichael@gmail.com',
-        license='MIT',
-        packages=find_packages(),
-        tests_require=['pytest', 'pytest-mock'],
-        entry_points = {
-            'console_scripts': ['breakzip=breakzip.command_line:main'],
-        },
-        zip_safe=False
+setup(
+    name="breakzip",
+    version="0.1",
+    description="",
+    author="Michael Terrell",
+    author_email="cwithmichael@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+    tests_require=["pytest"],
+    extras_require={"testing": ["pytest-mock"]},
+    entry_points={
+        "console_scripts": ["breakzip=breakzip.command_line:main"],
+    },
+    zip_safe=False,
 )
