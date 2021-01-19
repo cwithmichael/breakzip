b8d076da9ffb988c997d39ec6be60de1efbe890d
Michael Terrell <cwithmichael@gmail.com>
2021-01-19 08:01:40 -0600

    improvement: using argparse for command-line arguments

--- breakzip/command_line.py
+++ breakzip/command_line.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 import sys
+import argparse
 from zipfile import ZipFile
 from . import breakzip
 
 
 def main():
+    parser = argparse.ArgumentParser(description="""Smarter PKZIP Brute-Forcing""")
+    parser.add_argument(
+        "file_name", help="The name of the zip file. i.e. encrypted.zip"
+    )
+    parser.add_argument(
+        "file_ext", help="Known file extension in the zip file. i.e. jpg"
+    )
+    args = parser.parse_args()
+    file_name = args.file_name
     try:
-        file_name = sys.argv[1]
-        file_ext = sys.argv[2]
-    except IndexError:
-        raise SystemExit(f"Usage: {sys.argv[0]} <zip_filename> <file_ext>")
-
+        open(file_name, mode="r")
+    except:
+        print(f"Unable to open zip file: {file_name}")
+        sys.exit(1)
+    file_ext = args.file_ext
     enc_zip = ZipFile(file_name)
     file_sig = breakzip.FileSignature().get_file_sig(file_ext)
     if not file_sig:
@@ -21,7 +31,7 @@ def main():
     if not info:
         print(f"Couldn't find requested file type in archive")
         sys.exit(1)
-
+    print("Enter password: ")
     password = breakzip.find_password(enc_zip, file_sig, info)
     if password:
         print(f"Found it! -> {password}")
